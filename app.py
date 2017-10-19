d80d782f4ede77cf0c87928e96f5d9d3a9fbd5b4
kuldeepdadhich <getkuldeep.sh@gmail.com>
2017-10-19 21:44:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,13 @@ def processRequest(req):
         # "contextOut": [],
     }
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+app.run(debug=False, port=port, host='0.0.0.0')
+
 
 
     
